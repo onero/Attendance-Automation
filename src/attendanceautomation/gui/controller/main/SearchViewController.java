080981bab0ed0521101ec61be6523190028985ca
Adam Hansen <adamlars90@gmail.com>
2017-02-07 15:42:46 +0100

    Updated search view

--- src/attendanceautomation/gui/controller/main/SearchViewController.java
+++ src/attendanceautomation/gui/controller/main/SearchViewController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package attendanceautomation.gui.controller;
+package attendanceautomation.gui.controller.main;
 
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;

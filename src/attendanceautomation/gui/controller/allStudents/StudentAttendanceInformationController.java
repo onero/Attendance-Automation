e1feeb218a152fe78d80b3a164ee88f302c9f474
Adam Hansen <adamlars90@gmail.com>
2017-02-07 14:01:21 +0100

    Finished views from StudentAttendanceInformation

--- src/attendanceautomation/gui/controller/allStudents/StudentAttendanceInformationController.java
+++ src/attendanceautomation/gui/controller/allStudents/StudentAttendanceInformationController.java
@@ -5,10 +5,13 @@
  */
 package attendanceautomation.gui.controller.allStudents;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
@@ -29,7 +32,20 @@ public class StudentAttendanceInformationController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            createParentCheckBoxView();
+        } catch (Exception e) {
+        }
+
+    }
+
+    /**
+     * Creates the ParentCheckBoxView
+     */
+    private Node createParentCheckBoxView() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/attendanceautomation/gui/view/allStudents/ParentCheckBoxView.fxml"));
+        Node node = loader.load();
+        return node;
     }
 
 }

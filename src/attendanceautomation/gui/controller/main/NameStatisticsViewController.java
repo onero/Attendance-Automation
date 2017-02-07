bc19e6f12136d7a757cfba91cb7190d0b298e243
Sk0vgaard <Sk0vgaard@live.dk>
2017-02-07 16:16:34 +0100

    SeachBar
    Fixes

--- src/attendanceautomation/gui/controller/main/NameStatisticsViewController.java
+++ src/attendanceautomation/gui/controller/main/NameStatisticsViewController.java
@@ -6,11 +6,13 @@
 package attendanceautomation.gui.controller.main;
 
 import attendanceautomation.be.Student;
+import attendanceautomation.gui.controller.RootViewController;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -40,6 +42,19 @@ public class NameStatisticsViewController implements Initializable {
     public void setStudentInfo(Student student) {
         lblName.setText(student.getFullName());
         lblAttendance.setText("" + student.getAttendancePercentage() + " %");
+    }
+
+    /**
+     * Switches the center node to be the detailed student view
+     *
+     * @param event
+     */
+    @FXML
+    private void handleOpenStudentDetails(MouseEvent event) {
+        //If user double clicks. Switch view
+        if (event.getClickCount() == 2) {
+            RootViewController.getInstance().selectDetailedStudentView();
+        }
     }
 
 }
--- src/attendanceautomation/gui/controller/main/SearchViewController.java
+++ src/attendanceautomation/gui/controller/main/SearchViewController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package attendanceautomation.gui.controller.allStudents;
+package attendanceautomation.gui.controller.main;
 
 import attendanceautomation.be.Student;
 import attendanceautomation.gui.controls.AllStudentsNonAttendanceCell;

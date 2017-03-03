c53530070be39922484494dc75dabcdf6d8ebad7
Sk0vgaard <Sk0vgaard@live.dk>
2017-03-03 12:04:02 +0100

    Alertbox for logout - DONE

--- src/attendanceautomation/gui/controller/components/LogoutViewController.java
+++ src/attendanceautomation/gui/controller/components/LogoutViewController.java
@@ -6,12 +6,16 @@
 package attendanceautomation.gui.controller.components;
 
 import attendanceautomation.gui.controller.RootViewController;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
 
 /**
@@ -26,6 +30,7 @@ public class LogoutViewController implements Initializable {
 
     /**
      * Initializes the controller class.
+     *
      * @param url
      * @param rb
      */
@@ -36,12 +41,33 @@ public class LogoutViewController implements Initializable {
 
     /**
      * Logs off and returns to the login page.
+     *
      * @param event
      */
     @FXML
     private void processLogout(ActionEvent event) {
-        RootViewController rootViewController = RootViewController.getInstance();
-        rootViewController.logout();
+
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("WARNING");
+        alert.setHeaderText(" Press 'Ok' to logout. \n Press 'Cancel' to stay logged in.");
+        ButtonType yesButton = new ButtonType("Ok", ButtonBar.ButtonData.YES);
+        ButtonType cancelButton = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+        alert.getButtonTypes().setAll(yesButton, cancelButton);
+        alert.showAndWait().ifPresent(type -> {
+
+            if (type == yesButton) {
+                RootViewController rootViewController = RootViewController.getInstance();
+                rootViewController.logout();
+
+            }
+        });
+
+               
+
+        
+        
+        
+
     }
 
 }

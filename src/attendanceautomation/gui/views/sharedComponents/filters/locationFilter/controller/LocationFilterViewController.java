2b6f22986f204912ae90f6c30089a3f9c66b8747
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-03-14 12:56:20 +0100

    Made filter combobox for location

---
+++ src/attendanceautomation/gui/views/sharedComponents/filters/locationFilter/controller/LocationFilterViewController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendanceautomation.gui.views.sharedComponents.filters.locationFilter.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mathias
+ */
+public class LocationFilterViewController implements Initializable {
+
+    @FXML
+    private ComboBox<?> comboLocationFilter;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void handleLocationFilter(Event event) {
+
+    }
+
+}

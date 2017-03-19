2a0db49d5bab4f35e03ced0a4bc4856334827544
LindvedKrvang <rklindved@gmail.com>
2017-03-19 08:03:16 +0100

    Added functionality.
    The program is now getting two Strings with time. The first string is
    half an hour behind the currentTime. The second String is half an hour
    ahead of the currentTIme.
    
    OBS! In this commit the currentClass view does NOT return data!

--- src/attendanceautomation/bll/CurrentClassManager.java
+++ src/attendanceautomation/bll/CurrentClassManager.java
@@ -8,7 +8,10 @@ package attendanceautomation.bll;
 import attendanceautomation.be.NonAttendance;
 import attendanceautomation.be.Student;
 import attendanceautomation.dal.AttendanceAutomationDAOFacade;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -33,13 +36,25 @@ public class CurrentClassManager {
      * @return
      */
     public List<Student> getStudentsFromCurrentSchoolClass(int teacherID) {
-//        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//        Date date = new Date();
-//        System.out.println(dateFormat.format(date)); //2016/11/16 12:08:43
+        DateFormat dateFormatTime = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        String dateAsString = dateFormatTime.format(date);
+        String[] hoursAndMinutes = dateAsString.split(":");
+        String hours, minutes;
+        hours = hoursAndMinutes[0];
+        minutes = hoursAndMinutes[1];
 
-        String date = "2017-02-06 09:00";
+        String mockDate = "2017-02-06";
 
-        int schoolClassID = daoFacade.getSchoolClassID(teacherID, date);
+        String halfHourBefore = mockDate + " " + findHalfHourBefore(Integer.parseInt(hours), Integer.parseInt(minutes));
+        System.out.println("Before: " + halfHourBefore);
+        String halfHourAfter = mockDate + " " + findHalfHourAfter(Integer.parseInt(hours), Integer.parseInt(minutes));
+        System.out.println("After: " + halfHourAfter);
+
+//        //These lines are for when the actual date is needed.
+//        DateFormat dateFormatYear = new SimpleDateFormat("yyyy-MM-dd");
+//        String yearAsString = dateFormatYear.format(date);
+        int schoolClassID = daoFacade.getSchoolClassID(teacherID, dateAsString);
         if (schoolClassID == 0) {
             System.out.println("Failed to get schoolClassID");
         }
@@ -79,5 +94,62 @@ public class CurrentClassManager {
             }
         }
         return listOfStudentsPresent;
+    }
+
+    /**
+     * Takes the parsed time and converts it to a string that is half an hour
+     * behind time.
+     *
+     * @param hours
+     * @param minutes
+     * @return
+     */
+    private String findHalfHourBefore(int hours, int minutes) {
+        String halfHourBefore = "";
+        minutes -= 30;
+        if (minutes <= 0) {
+            hours--;
+            minutes = 60 + minutes;
+        }
+        halfHourBefore = formatTime(halfHourBefore, hours, minutes);
+        return halfHourBefore += minutes;
+    }
+
+    /**
+     * Takes the parsed times and converts it to string that is half an hour
+     * ahead of time.
+     *
+     * @param hours
+     * @param minutes
+     * @return
+     */
+    private String findHalfHourAfter(int hours, int minutes) {
+        String halfHourAfter = "";
+        minutes += 30;
+        if (minutes > 60) {
+            hours++;
+            minutes -= 60;
+        }
+        halfHourAfter = formatTime(halfHourAfter, hours, minutes);
+        return halfHourAfter += minutes;
+    }
+
+    /**
+     * Formats the given parameters to be a valid time of day.
+     *
+     * @param time
+     * @param hours
+     * @param minutes
+     * @return
+     */
+    private String formatTime(String time, int hours, int minutes) {
+        if (hours < 10) {
+            time = "0";
+        }
+        time += hours + ":";
+        if (minutes < 10) {
+            time += 0;
+        }
+        return time;
     }
 }
